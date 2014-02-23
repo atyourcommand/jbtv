9d6687c37737717277a7f83d691a706b152c4882
John Brunskill <johnlouisbrunskill@gmail.com>
2014-02-23 12:52:26 +1100

    First commit of project

---
+++ application/models/latest_model.php
@@ -0,0 +1,13 @@
+<?php
+
+//API Key: ba5a09dba76b1c3875e487780468ef93
+
+class Latest_model extends CI_Model
+{
+	function Latest_model()
+	  {
+		parent::__construct();
+	  }
+
+}
+?>
---
+++ application/views/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="utf-8">
+<title>JB TV</title>
+</head>
+<body>
+Welcome to the JB TV
+</body>
+</html>
